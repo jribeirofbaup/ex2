7b12e67ba97ade127a0793e5a4452e9444b11edf
Jorge Ribeiro <jorge.ribeiro@fraunhofer.pt>
2017-03-16 22:05:55 +0000

    initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('.btn').on('click', function() {
+
+        // $('.list').children().first().nextAll().addClass('active');
+        $('.list li').fadeToggle();
+    });
+
+
+
+});
